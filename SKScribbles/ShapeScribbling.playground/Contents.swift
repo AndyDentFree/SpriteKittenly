16300c34a496c16b358edcaddf553c0210d249af
Andy Dent <dent@oofile.com.au>
2018-05-22 00:32:48 +0800

    ShapeScribbling playground - very slow approach recreating the SKShapeNode each move

--- SKScribbles/ShapeScribbling.playground/Contents.swift
+++ SKScribbles/ShapeScribbling.playground/Contents.swift
@@ -5,52 +5,39 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode!
-    private var spinnyNode : SKShapeNode!
-    
+    private var linesNode : SKShapeNode? = nil
+    private var pointsDrawn = [CGPoint]()
+  
     override func didMove(to view: SKView) {
-        // Get label node from scene and store it for use later
-        label = childNode(withName: "//helloLabel") as? SKLabelNode
-        label.alpha = 0.0
-        let fadeInOut = SKAction.sequence([.fadeIn(withDuration: 2.0),
-                                           .fadeOut(withDuration: 2.0)])
-        label.run(.repeatForever(fadeInOut))
-        
-        // Create shape node to use during mouse interaction
-        let w = (size.width + size.height) * 0.05
-        
-        spinnyNode = SKShapeNode(rectOf: CGSize(width: w, height: w), cornerRadius: w * 0.3)
-        spinnyNode.lineWidth = 2.5
-        
-        let fadeAndRemove = SKAction.sequence([.wait(forDuration: 0.5),
-                                               .fadeOut(withDuration: 0.5),
-                                               .removeFromParent()])
-        spinnyNode.run(.repeatForever(.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-        spinnyNode.run(fadeAndRemove)
+
     }
     
     func touchDown(atPoint pos : CGPoint) {
-        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
-        
-        n.position = pos
-        n.strokeColor = SKColor.green
-        addChild(n)
+        pointsDrawn = [pos]
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        guard let n = self.spinnyNode.copy() as? SKShapeNode else { return }
-        
-        n.position = pos
-        n.strokeColor = SKColor.blue
-        addChild(n)
+      guard pos != pointsDrawn.last else { return }
+      pointsDrawn.append(pos)
+      updatePath()
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
-        
-        n.position = pos
-        n.strokeColor = SKColor.red
-        addChild(n)
+      guard pos != pointsDrawn.last else { return }
+      pointsDrawn.append(pos)
+      updatePath()
+    }
+  
+    func updatePath() {
+      
+      // start with completely re-creating the node. Horribly slow
+      if linesNode != nil {
+        removeChildren(in: [linesNode!])
+      }
+      linesNode = SKShapeNode(points:&pointsDrawn, count:pointsDrawn.count)
+      linesNode!.lineWidth = 1
+      linesNode!.strokeColor = SKColor.green
+      addChild(linesNode!)
     }
     
     override func mouseDown(with event: NSEvent) {

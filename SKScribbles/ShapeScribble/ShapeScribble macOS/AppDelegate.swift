c1af1868ddfa853636282a59ce092dd0fca21bbc
Andy Dent <dent@oofile.com.au>
2018-05-23 14:36:25 +0800

    ShapeScribble - new SK app added for iOS and MacOS

---
+++ SKScribbles/ShapeScribble/ShapeScribble macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ShapeScribble macOS
+//
+//  Created by Andrew Dent on 23/5/18.
+//  Copyright © 2018 Touchgram Pty Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+

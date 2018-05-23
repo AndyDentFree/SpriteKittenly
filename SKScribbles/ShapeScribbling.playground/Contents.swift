237e08c809568bd7055bcde45fdea066ffe71e8b
Andy Dent <dent@oofile.com.au>
2018-05-23 14:36:07 +0800

    ShapeScribbling playground - adding a new SKShapeNode each move for just a two-point segment but with optimisation needing to move more so doesn't generate trivial ones

--- SKScribbles/ShapeScribbling.playground/Contents.swift
+++ SKScribbles/ShapeScribbling.playground/Contents.swift
@@ -1,39 +1,48 @@
-//: A SpriteKit based Playground
+//: This version generates a new SKShapeNode for each line moving slightly from the previous point
+//: Is **very** sensitive to epsilon values, starting at 4 gets nearly real-time tracking without looking too jagged
 
 import PlaygroundSupport
 import SpriteKit
 
+extension CGPoint {
+  func notCloseTo(_ rhs:CGPoint) -> Bool {
+    let epsilon : CGFloat = 4.0
+    return abs(x - rhs.x) > epsilon || abs(y - rhs.y) > epsilon
+  }
+}
+
+
 class GameScene: SKScene {
     
-    private var linesNode : SKShapeNode? = nil
     private var pointsDrawn = [CGPoint]()
+    private var tempNodes = [SKShapeNode]()
   
     override func didMove(to view: SKView) {
 
     }
     
     func touchDown(atPoint pos : CGPoint) {
-        pointsDrawn = [pos, pos]  // insert a dummy so always two points
+        pointsDrawn = [pos]
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-      guard pos != pointsDrawn.last else { return }
+      guard pos.notCloseTo(pointsDrawn.last!) else { return }
       updatePath(pos)
     }
     
     func touchUp(atPoint pos : CGPoint) {
-      guard pos != pointsDrawn.last else { return }
+      guard pos.notCloseTo(pointsDrawn.last!) else { return }
       updatePath(pos)
     }
   
     func updatePath(_ pos : CGPoint) {
-      pointsDrawn[0] = pointsDrawn[1]
-      pointsDrawn[1] = pos
+      pointsDrawn.append(pos)
       // add new node of two points
-      linesNode = SKShapeNode(points:&pointsDrawn, count:pointsDrawn.count)
-      linesNode!.lineWidth = 1
-      linesNode!.strokeColor = SKColor.green
-      addChild(linesNode!)
+      var linesNode = SKShapeNode(points:&pointsDrawn[pointsDrawn.count-2], count:2)
+      linesNode.lineWidth = 1
+      linesNode.strokeColor = SKColor.yellow
+      addChild(linesNode)
+      //tempNodes.append(linesNode)  // WHOA! this makes it slow down massively
     }
     
     override func mouseDown(with event: NSEvent) {
@@ -46,6 +55,13 @@ class GameScene: SKScene {
     
     override func mouseUp(with event: NSEvent) {
         touchUp(atPoint: event.location(in: self))
+      
+        // this has no perceivable performance impact removeChildren(in: tempNodes)
+        var finishedPts = SKShapeNode(splinePoints:&pointsDrawn, count:pointsDrawn.count)
+        finishedPts.lineWidth = 1
+        finishedPts.strokeColor = SKColor.green
+        finishedPts.glowWidth = 2.0
+        addChild(finishedPts)
     }
     
     override func update(_ currentTime: TimeInterval) {

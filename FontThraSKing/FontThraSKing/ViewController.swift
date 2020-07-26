77385e8584420efef1e9a91f32df346c12ffce6b
Andy Dent <dent@oofile.com.au>
2020-07-26 10:40:20 +0800

    FontThraSKing - replicating the crash "unrecognized selector sent to instance"
    Basic UI
    
    MainStoryboard
    - ViewController scene
      - added Stack with two labels and a button
      - connected labels to outlets
      - connected button to onPickFont IBAction
    
    Adding Font Picking
    
    ViewController.swift
    - added demoText string property (anticipate changing text in future)
    - added pickedFontDesc property
    - updateDisplays added to set text and try to set as attributedText on one, if pickedFontDesc
    
    - made a UIFontPickerViewControllerDelegate
      - added ...DidPick setting pickedFontDesc & calling update

--- FontThraSKing/FontThraSKing/ViewController.swift
+++ FontThraSKing/FontThraSKing/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDisplay: UILabel!
+    @IBOutlet weak var textDisplaySerialised: UILabel!
+    
+    var demoText = "Demo text to display using font picked"
+    var pickedFontDesc: UIFontDescriptor? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateDisplays()
+    }
+    
+    /// Two labels show the same text
+    func updateDisplays() {
+        textDisplaySerialised.text = demoText
+
+        if let fd = pickedFontDesc {
+            let attStr = NSMutableAttributedString(string:demoText)
+            let allRange = NSRange(location: 0, length: attStr.length)
+            attStr.addAttribute(NSAttributedString.Key.font, value:fd, range:allRange)
+            textDisplay.attributedText = NSAttributedString(attributedString:attStr)
+        } else {
+            textDisplay.text = demoText
+        }
+        
     }
 
-
+    /// Displays the font picker
+    @IBAction func onPickFont(_ sender: UIButton) {
+        let fc = UIFontPickerViewController.Configuration()
+        fc.includeFaces = true
+        let fp = UIFontPickerViewController(configuration: fc)
+        fp.delegate = self
+        self.present(fp, animated: true, completion: nil)
+    }
+    
 }
 
+
+extension ViewController : UIFontPickerViewControllerDelegate {
+    func fontPickerViewControllerDidPickFont(_ viewController: UIFontPickerViewController) {
+        pickedFontDesc = viewController.selectedFontDescriptor
+        updateDisplays()
+    }
+}
--- FontThraSKing/README.md
+++ FontThraSKing/README.md
@@ -7,3 +7,5 @@ This is closely related to the sibling project StyledTextOverSK but specifically
 So, although most of the logic is about iOS font creation, it includes SpriteKit and display over it partly in case there's an interaction.
 
 It also (maybe later) sneaks in some ideas of rendering fonts into sprites with a comparison as to how the typography works out.
+
+The _Thrashing_ part of the name is about the intent to automatically generate a range of font sizes and styles and iterate them to see if any combos cause crashes.

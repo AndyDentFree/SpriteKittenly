d50dcfdd504a0007e024bfe8f88af92dbbf22d0e
Andy Dent <me@andydent.com>
2025-03-11 15:42:06 +0800

    ToP3OrNotP3 - creation by copying

---
+++ ToP3orNotToP3/README.md
@@ -0,0 +1,7 @@
+# ToP3orNotToP3
+
+I was evaluating using extended colors in [Touchgram][tg] and [Purrticles][p1] because the iOS colour picker allows picking 
+
+
+[tg]: https://www.touchgram.com
+[p1]: https://www.touchgram.com/purrticles
---
+++ ToP3orNotToP3/ToP3orNotToP3/LayoutSensingSKView.swift
@@ -0,0 +1,29 @@
+//
+//  LayoutSensingSKView.swift
+//  ToP3orNotToP3
+//
+//  Created by Andrew Dent on 3/12/2024.
+//
+
+
+import SpriteKit
+import SwiftUI
+
+// helper class so can invoke a lambda onLayout, typically when SKView has been resized
+class LayoutSensingSKView: SKView {
+    
+    @MainActor
+    var onLayout: ((SKView) -> Void)!
+    
+    #if os(iOS)
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        onLayout?(self)
+    }
+    #elseif os(macOS)
+    override func layout() {
+        super.layout()
+        onLayout?(self)
+    }
+    #endif
+}
